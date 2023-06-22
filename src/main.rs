56df287d6ff35224c3b0b27ac310409ebef029b3
Chandler Chilvers <cwchilvers@gmail.com>
2023-06-22 10:24:05 -0700

    Modified main.rs

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,46 @@
 extern crate rocket;
 
 use rocket::fs::NamedFile;
-use std::path::{PathBuf};
-
-#[get("/")]
-async fn index() -> Option<NamedFile> {
-    NamedFile::open(PathBuf::from("public/html/index.html")).await.ok()
-}
+use std::path::PathBuf;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+        .mount("/", routes![index, music, games, about])
+        .mount("/music", routes![film])
+        .register("/", catchers![not_found])
+}
+
+#[get("/")]
+async fn index() -> Option<NamedFile> {
+    serve_static_file("index.html").await
+}
+
+#[get("/music")]
+async fn music() -> Option<NamedFile> {
+    serve_static_file("music.html").await
+}
+
+#[get("/film")]
+async fn film() -> Option<NamedFile> {
+    serve_static_file("film.html").await
+}
+
+#[get("/games")]
+async fn games() -> Option<NamedFile> {
+    serve_static_file("games.html").await
+}
+
+#[get("/about")]
+async fn about() -> Option<NamedFile> {
+    serve_static_file("about.html").await
+}
+
+#[catch(404)]
+async fn not_found() -> Option<NamedFile> {
+    serve_static_file("404.html").await
+}
+
+async fn serve_static_file(file_name: &str) -> Option<NamedFile> {
+    NamedFile::open(PathBuf::from(format!("public/html/{}", file_name))).await.ok()
 }
