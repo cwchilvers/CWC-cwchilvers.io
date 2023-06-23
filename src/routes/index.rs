b172361dbc56079262dc9fa8ffc8be0a0399d508
Chandler Chilvers <cwchilvers@gmail.com>
2023-06-22 20:53:57 -0700

    Removed more redundencies

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,36 +1,26 @@
 use rocket::fs::NamedFile;
 use std::path::PathBuf;
 
-#[get("/")]
-pub async fn index() -> Option<NamedFile> {
-    serve_static_file("index.html").await
+macro_rules! static_file_route {
+    ($route:expr, $fn_name:ident, $file:expr) => {
+        #[get($route)]
+        pub async fn $fn_name() -> Option<NamedFile> {
+            serve_static_file(concat!("templates/html/", $file, ".html")).await
+        }
+    };
 }
 
-#[get("/music")]
-pub async fn music() -> Option<NamedFile> {
-    serve_static_file("music.html").await
-}
-
-#[get("/film")]
-pub async fn film() -> Option<NamedFile> {
-    serve_static_file("film.html").await
-}
-
-#[get("/games")]
-pub async fn games() -> Option<NamedFile> {
-    serve_static_file("games.html").await
-}
-
-#[get("/about")]
-pub async fn about() -> Option<NamedFile> {
-    serve_static_file("about.html").await
-}
+static_file_route!("/", index, "index");
+static_file_route!("/music", music, "music");
+static_file_route!("/film", film, "film");
+static_file_route!("/games", games, "games");
+static_file_route!("/about", about, "about");
 
 #[catch(404)]
 pub async fn not_found() -> Option<NamedFile> {
-    serve_static_file("404.html").await
+    serve_static_file("templates/html/404.html").await
 }
 
-async fn serve_static_file(file_name: &str) -> Option<NamedFile> {
-    NamedFile::open(PathBuf::from(format!("templates/html/{}", file_name))).await.ok()
+async fn serve_static_file(file_path: &str) -> Option<NamedFile> {
+    NamedFile::open(PathBuf::from(file_path)).await.ok()
 }
